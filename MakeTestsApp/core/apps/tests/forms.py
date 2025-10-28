3bfa0b5955d9fe759a46c63abeca8bbb8d30bec3
YevgeniyPD <128799036+YevgeniyPD@users.noreply.github.com>
2025-10-28 15:33:17 +0300

    update test edit, added update_test_info, add_question in views.py. Added new forms in forms.py

--- MakeTestsApp/core/apps/tests/forms.py
+++ MakeTestsApp/core/apps/tests/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Test
+from .models import Test, Question, Answer
 
 
 class TestAdminForm(forms.ModelForm):
@@ -10,6 +10,7 @@ class TestAdminForm(forms.ModelForm):
         if tags and tags.count() > 4:
             raise forms.ValidationError('Вы можете выбрать не более 4 тегов.')
         return cleaned_data
+
 
 class AddTestForm(forms.ModelForm):
     class Meta:
@@ -30,3 +31,56 @@ class AddTestForm(forms.ModelForm):
         if len(title) < 3:
             raise forms.ValidationError("Название должно быть не короче 3 символов.")
         return title
+
+
+class TestEditForm(forms.ModelForm):
+    class Meta:
+        model = Test
+        fields = ['title', 'content']
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'test-info-edit__title-input span-title',
+                'autocomplete': 'off',
+                'placeholder': 'Введите название теста'
+            }),
+            'content': forms.Textarea(attrs={
+                'class': 'test-info-edit__content-textarea span-input',
+                'placeholder': 'Описание теста'
+            }),
+        }
+
+
+class TestStatusEditForm(forms.ModelForm):
+    class Meta:
+        model = Test
+        fields = ['status']
+        widgets = {
+            'status': forms.Select(attrs={'class': 'test-status-select'})
+        }
+
+
+class AddQuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = ['text', 'type']
+        widgets = {
+            'text': forms.TextInput(attrs={
+                'class': 'questions-edit__new-question-input span-input',
+                'placeholder': 'Введите текст вопроса'
+            }),
+            'type': forms.Select(attrs={
+                'class': 'questions-edit__type-selector'
+            }),
+        }
+
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ['text', 'flag']
+        widgets = {
+            'text': forms.TextInput(attrs={
+                'class': 'questions-edit__answer-input span-answer-input',
+                'placeholder': 'Ответ'
+            }),
+        }
