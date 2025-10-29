966bd9371068ad286ef57c55caa8cb7aa2143bbb
YevdmiWork <128799036+YevdmiWork@users.noreply.github.com>
2025-10-29 12:52:38 +0300

    Revert "Merge develop into master"

--- MakeTestsApp/core/apps/tests/urls.py
+++ MakeTestsApp/core/apps/tests/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import AddTest, AllTests, TestView, TestEdit, TestRun, update_test_info, add_question
+from django.conf import settings
+
+from .views import AddTest, AllTests, TestView, TestEdit, TestRun
 
 app_name = "tests"
 
@@ -8,6 +10,4 @@ urlpatterns = [path('tests/', AllTests.as_view(), name='home'),
                path('tests/<slug:test_slug>/', TestView.as_view(), name='test_view'),
                path('tests/<slug:test_slug>/edit/', TestEdit.as_view(), name='test_edit'),
                path('tests/<slug:test_slug>/run/', TestRun.as_view(), name='test_run'),
-               path('update-test-info/', update_test_info, name='update_test_info'),
-               path('add-question/', add_question, name='add_question'),
                ]
--- MakeTestsApp/core/apps/tests/views.py
+++ MakeTestsApp/core/apps/tests/views.py
@@ -1,12 +1,10 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import JsonResponse
 from django.shortcuts import redirect
-from django.template.loader import render_to_string
-from django.views.decorators.http import require_POST
+from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
 
-from .forms import AddTestForm, TestEditForm, TestStatusEditForm, AddQuestionForm, AnswerForm
-from .models import Test, Question
+from .forms import AddTestForm
+from .models import Test
 from .services import create_test
 
 
@@ -43,6 +41,14 @@ class TestRun(BaseTestView):
         return Test.objects.with_fields()
 
 
+class TestEdit(BaseTestView):
+    context_object_name = 'test'
+    template_name = 'tests/test_edit.html'
+
+    def get_queryset(self):
+        return Test.objects.with_test_status().with_test_content()
+
+
 class AddTest(LoginRequiredMixin, CreateView):
     model = Test
     template_name = 'tests/add_test.html'
@@ -52,72 +58,3 @@ class AddTest(LoginRequiredMixin, CreateView):
         test = create_test(self.request.user, form.cleaned_data)
         return redirect(test.get_edit_url())
 
-
-class TestEdit(BaseTestView):
-    model = Test
-    context_object_name = 'test'
-    template_name = 'tests/test_edit.html'
-
-    def get_queryset(self):
-        return Test.objects.with_test_status().with_test_content()
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['test_edit_form'] = TestEditForm(instance=self.object)
-        context['test_status_form'] = TestStatusEditForm(instance=self.object)
-        context['add_question_form'] = AddQuestionForm()
-        context['add_answer_form'] = AnswerForm()
-        context['questions'] = Question.objects.with_answers().filter(test=self.object)
-        context['type_of_question_choices'] = Question.QuestionType.choices
-        return context
-
-
-@require_POST
-def update_test_info(request):
-    test_id = request.POST.get('test_id')
-    title = request.POST.get('title')
-    content = request.POST.get('content')
-
-    if not test_id:
-        return JsonResponse({'success': False, 'error': 'Не указан ID теста'})
-
-    try:
-        test = Test.objects.get(pk=test_id, author=request.user)
-    except Test.DoesNotExist:
-        return JsonResponse({'success': False, 'error': 'Тест не найден или нет прав'})
-
-    if title is not None:
-        test.title = title
-    if content is not None:
-        test.content = content
-
-    test.save(update_fields=['title', 'content', 'time_update'])
-
-    return JsonResponse({'success': True, 'message': 'Изменения сохранены'})
-
-
-@require_POST
-def add_question(request):
-    test_id = request.POST.get('test_id')
-    text = request.POST.get('text')
-    question_type = request.POST.get('type')
-
-    if not test_id or not text:
-        return JsonResponse({'success': False, 'error': 'Не указаны обязательные поля'})
-
-    try:
-        test = Test.objects.get(pk=test_id, author=request.user)
-    except Test.DoesNotExist:
-        return JsonResponse({'success': False, 'error': 'Тест не найден или нет прав'})
-
-    question = Question.objects.create(test=test, text=text, type=question_type)
-    question_number = test.related_test.count()
-
-    question_html = render_to_string('tests/question_block.html', {
-        'question': question,
-        'question_number': question_number,
-        'type_of_question_choices': Question.QuestionType.choices,
-        'answer_form': AnswerForm(),
-    }, request=request)
-
-    return JsonResponse({'success': True, 'question_html': question_html})
