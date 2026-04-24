cea6cd3d82077c1d32480cfa7da6b57676b84427
YevgeniyPD <128799036+YevgeniyPD@users.noreply.github.com>
2026-04-24 16:30:41 +0300

    add register user view, form, services and ui(html, css)

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from .forms import RegisterUserForm
+from .services import register_user
 
 
+class RegisterUser(CreateView):
+    form_class = RegisterUserForm
+    template_name = 'users/registration.html'
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        self.object = register_user(self.request, form)
+        return redirect('pages:home')
