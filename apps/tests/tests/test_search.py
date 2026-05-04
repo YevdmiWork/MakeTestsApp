e1c61b7a5615bb997573d69be3c7536f4d1784b3
YevgeniyPD <128799036+YevgeniyPD@users.noreply.github.com>
2026-05-04 17:36:05 +0300

    add tests for search and sort in AllTests view. add pytest.init

---
+++ apps/tests/tests/test_search.py
@@ -0,0 +1,36 @@
+from apps.tests.constants.search import SEARCH_PARAM
+from apps.tests.views.pages import AllTests
+from unittest.mock import Mock
+
+
+def test_invalid_sort(rf):
+    request = rf.get('/tests/?sort_by=wrong')
+    view = AllTests()
+    view.request = request
+
+    assert view.get_current_sort() == '-completion'
+
+
+def test_apply_search(rf):
+    request = rf.get(f'/tests/?{SEARCH_PARAM}=iq tests')
+    view = AllTests()
+    view.request = request
+
+    qs = Mock()
+    qs.search.return_value = qs
+
+    view.apply_search(qs)
+
+    qs.search.assert_called_once_with('iq tests')
+
+
+def test_empty_search(rf):
+    request = rf.get(f'/tests/?{SEARCH_PARAM}=')
+    view = AllTests()
+    view.request = request
+
+    qs = Mock()
+
+    view.apply_search(qs)
+
+    qs.search.assert_not_called()
