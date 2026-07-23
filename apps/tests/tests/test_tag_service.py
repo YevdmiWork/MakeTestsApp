43b3d1fa587d72c332a63b8bf1cdb3784307325d
YevgeniyPD <128799036+YevgeniyPD@users.noreply.github.com>
2026-07-23 16:19:45 +0300

    add tests for tag services

---
+++ apps/tests/tests/test_tag_service.py
@@ -0,0 +1,35 @@
+import pytest
+
+from ..services import tag as tag_service
+
+
+@pytest.mark.django_db
+def test_add_tag_to_test(test, user, tag):
+    result = tag_service.add_tag_to_test(
+        test=test,
+        user=user,
+        tag_id=tag.id,
+    )
+
+    assert result == tag
+
+    test.refresh_from_db()
+
+    assert test.tag.filter(id=tag.id).exists()
+
+
+@pytest.mark.django_db
+def test_remove_tag_from_test(test, user, tag):
+    test.tag.add(tag)
+
+    result = tag_service.remove_tag_from_test(
+        test=test,
+        user=user,
+        tag_id=tag.id,
+    )
+
+    assert result == tag
+
+    test.refresh_from_db()
+
+    assert not test.tag.filter(id=tag.id).exists()
