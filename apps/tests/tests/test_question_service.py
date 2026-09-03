3f1e13ff1d99a487535fd2f98bfb379d392d57c3
YevgeniyPD <128799036+YevgeniyPD@users.noreply.github.com>
2026-09-03 15:33:55 +0300

    add tests for update question type service and validator

--- apps/tests/tests/test_question_service.py
+++ apps/tests/tests/test_question_service.py
@@ -1,5 +1,6 @@
 import pytest
 
+from ..models.question import Question
 from ..services import question as question_service
 
 @pytest.mark.django_db
@@ -15,3 +16,37 @@ def test_create_question(test, user):
     assert result.text == text
     assert result.test is test
     assert result.pk is not None
+
+
+@pytest.mark.django_db
+def test_update_question_text(test, user, question):
+    new_text = '2 - 2 ='
+
+    result = question_service.update_question_text(
+        question=question,
+        user=user,
+        text=new_text,
+    )
+
+    assert result is question
+
+    question.refresh_from_db()
+
+    assert question.text == new_text
+
+
+@pytest.mark.django_db
+def test_update_question_type(question, user):
+    question_type = Question.QuestionType.SINGLE_CHOICE
+
+    result = question_service.update_question_type(
+        question=question,
+        user=user,
+        question_type=question_type,
+    )
+
+    assert result is question
+
+    question.refresh_from_db()
+
+    assert question.type == question_type
--- apps/tests/tests/test_question_validator.py
+++ apps/tests/tests/test_question_validator.py
@@ -2,7 +2,8 @@ import pytest
 
 from ..constants.limits import QuestionLimits
 from ..exceptions import AppValidationError
-from ..validators.question import validate_question_text
+from ..models.question import Question
+from ..validators.question import validate_question_text, validate_question_type
 
 
 class TestValidateQuestion:
@@ -29,3 +30,48 @@ class TestValidateQuestion:
                 text=text,
                 max_length=QuestionLimits.TITLE_MAX_LENGTH,
             )
+
+
+class TestValidateQuestionText:
+    def test_empty_text(self):
+        with pytest.raises(AppValidationError):
+            validate_question_text(
+                text='',
+                max_length=QuestionLimits.TITLE_MAX_LENGTH,
+            )
+
+    def test_equal_max(self):
+        text = 'a' * QuestionLimits.TITLE_MAX_LENGTH
+
+        validate_question_text(
+            text=text,
+            max_length=QuestionLimits.TITLE_MAX_LENGTH,
+        )
+
+    def test_greater_than_max(self):
+        text = 'a' * (QuestionLimits.TITLE_MAX_LENGTH + 1)
+
+        with pytest.raises(AppValidationError):
+            validate_question_text(
+                text=text,
+                max_length=QuestionLimits.TITLE_MAX_LENGTH,
+            )
+
+
+class TestValidateQuestionType:
+    def test_empty_type(self):
+        with pytest.raises(AppValidationError):
+            validate_question_type(
+                question_type='',
+            )
+
+    def test_wrong_type(self):
+        with pytest.raises(AppValidationError):
+            validate_question_type(
+                question_type='false_type',
+            )
+
+    def test_valid_type(self):
+        validate_question_type(
+            question_type=Question.QuestionType.SINGLE_CHOICE,
+        )
